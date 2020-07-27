de9adc6e33fc88fc57983ac1886b24c8481b2d1f
C.C. Leonard <67921480+ccleonard@users.noreply.github.com>
2020-07-27 15:31:36 -0400

    Added the warning and list

---
+++ durham-delivers/src/components/how.js
@@ -0,0 +1,31 @@
+import React from "react"
+
+const HowTo = () => (
+  <div
+    style={{
+      maxWidth: `1080px`,
+      padding: `0 15px`,
+      margin: `130px auto`
+    }}
+  >
+    <h2>
+      How it Works
+    </h2>
+    <ol>
+      <li>
+        Find a time and place most convient to you to order a meal
+      </li>
+      <li>
+        Order your meal from a participating Durham restaurant
+      </li>
+      <li>
+        Pick up your order at the coresponding Durham Park
+      </li>
+      <li>
+        Enjoy!
+      </li>
+    </ol>
+  </div>
+)
+
+export default HowTo
---
+++ durham-delivers/src/components/warning.js
@@ -0,0 +1,28 @@
+import React from "react"
+
+const Warning = () => (
+  <div
+    style={{
+      borderTop: `3px solid #BE3434`,
+      borderBottom: `3px solid #BE3434`,
+      background: `#FFF4F4`,
+      marginTop: `120px`,
+      textAlign: `center`
+    }}
+  >
+    <p
+    style={{
+      padding: `36px 0`,
+      fontFamily: `'Lato', 'Helvetica', sans-serif`,
+      fontWeight: `600`,
+      fontSize: `27px`,
+      letterSpacing: `0.01em`,
+      color: `#BE3434`
+    }}
+    >
+      Due to COVID–19 please be caution by staying 6ft apart and wearing your masks when picking up
+    </p>
+  </div>
+)
+
+export default Warning
