02dcae56f4f8151811f0d1ef182729770cf9e19c
C.C. Leonard <67921480+ccleonard@users.noreply.github.com>
2020-07-28 13:21:55 -0400

    Added locations

---
+++ durham-delivers/src/components/locations.js
@@ -0,0 +1,144 @@
+import React from "react"
+import Background from "../images/fork-bkgr.jpg"
+import Park1 from "../images/park-1.png"
+
+const Locations = () => (
+  <div
+    style={{
+      backgroundImage: `url(${Background})`,
+      backgroundSize: `cover`,
+      padding: `100px 0`
+    }}
+  >
+    <div
+      style={{
+        padding: `0 15px`,
+        maxWidth: `1080px`,
+        margin: `0 auto`,
+      }}
+    >
+      <h2>
+        Local Pick Up
+      </h2>
+      <div
+        style={{
+          display: `flex`,
+          flexWrap: `wrap`,
+          justifyContent: `space-between`
+        }}
+      >
+        <div
+          style={{
+            width: `calc(33% - 20px)`,
+            marginBottom: `50px`
+          }}
+        >
+          <div
+            style={{
+              backgroundImage: `url(${Park1})`,
+              width: `342px`,
+              height: `342px`,
+              backgroundSize: `cover`,
+              backgroundPosition: `center`,
+              backgroundRepeat: `no-repeat`
+            }}
+          >
+          </div>
+          <div style={{
+            padding: `10px 0 10px 8px`
+          }}>
+            <h4>
+              Park One
+            </h4>
+            <p>
+              501 Foster St, Durham, NC 27701
+            </p>
+          </div>
+        </div>
+        <div
+          style={{
+            width: `calc(33% - 20px)`,
+          }}
+        >
+          <div
+            style={{
+              backgroundImage: `url(${Park1})`,
+              width: `342px`,
+              height: `342px`,
+              backgroundSize: `cover`,
+              backgroundPosition: `center`,
+              backgroundRepeat: `no-repeat`
+            }}
+          >
+          </div>
+          <div style={{
+            padding: `10px 0 10px 8px`
+          }}>
+            <h4>
+              Park Two
+            </h4>
+            <p>
+              501 Foster St, Durham, NC 27701
+            </p>
+          </div>
+        </div>
+        <div
+          style={{
+            width: `calc(33% - 20px)`,
+          }}
+        >
+          <div
+            style={{
+              backgroundImage: `url(${Park1})`,
+              width: `342px`,
+              height: `342px`,
+              backgroundSize: `cover`,
+              backgroundPosition: `center`,
+              backgroundRepeat: `no-repeat`
+            }}
+          >
+          </div>
+          <div style={{
+            padding: `10px 0 10px 8px`
+          }}>
+            <h4>
+              Park Two
+            </h4>
+            <p>
+              501 Foster St, Durham, NC 27701
+            </p>
+          </div>
+        </div>
+        <div
+          style={{
+            width: `calc(33% - 20px)`,
+          }}
+        >
+          <div
+            style={{
+              backgroundImage: `url(${Park1})`,
+              width: `342px`,
+              height: `342px`,
+              backgroundSize: `cover`,
+              backgroundPosition: `center`,
+              backgroundRepeat: `no-repeat`
+            }}
+          >
+          </div>
+          <div style={{
+            padding: `10px 0 10px 8px`
+          }}>
+            <h4>
+              Park Two
+            </h4>
+            <p>
+              501 Foster St, Durham, NC 27701
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+export default Locations
--- durham-delivers/src/pages/index.js
+++ durham-delivers/src/pages/index.js
@@ -1,16 +1,17 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Warning from "../components/warning"
 import HowTo from "../components/how"
+import Locations from "../components/locations"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Warning />
     <HowTo />
+    <Locations />
   </Layout>
 )
 
