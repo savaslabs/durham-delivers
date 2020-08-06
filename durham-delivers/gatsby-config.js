b6c6ea0b2dbaf72ff8185622d9ba1722ca9a21c1
C.C. Leonard <67921480+ccleonard@users.noreply.github.com>
2020-08-06 08:02:12 -0400

    Updated gatsby-config.js

--- durham-delivers/gatsby-config.js
+++ durham-delivers/gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: '/durham-delivers',
+  pathPrefix: '/durham-delivers/docs',
   siteMetadata: {
     title: `Durham Delivers`,
     description: `Duke Delivers partners with local restaurants to create community food deliveries to parks and other spaces in Durham`,
