3af3c6ffb70ae50fbb02ffd47bedef5f772a571f
C.C. Leonard <67921480+ccleonard@users.noreply.github.com>
2020-08-06 08:37:15 -0400

    Remove unnecessary files

--- durham-delivers/gatsby-config.js
+++ durham-delivers/gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: '/durham-delivers/docs',
+  pathPrefix: '/durham-delivers',
   siteMetadata: {
     title: `Durham Delivers`,
     description: `Duke Delivers partners with local restaurants to create community food deliveries to parks and other spaces in Durham`,
