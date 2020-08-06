80d07fd09f6ad6bfb17e8fdf464028327a35dd96
C.C. Leonard <67921480+ccleonard@users.noreply.github.com>
2020-08-06 07:44:51 -0400

    Removing static files

---
+++ durham-delivers/src/components/how.js
@@ -0,0 +1,25 @@
+import React from "react"
+
+const HowTo = () => (
+  <div className="how__container" id="how-it-works">
+    <h2>
+      How it Works
+    </h2>
+    <ol>
+      <li>
+        Find out where and when your favorite local restaurant delivers to a location near you.<br />See the <a href="#calendar">calendar below</a>.
+      </li>
+      <li>
+        Place your order with the restaurant. Let them know it’s for a Durham Delivers event.
+      </li>
+      <li>
+        Pick up your food at the designated time and location and enjoy it proudly, knowing you’re safely supporting local, independent restaurants!
+      </li>
+      <li>
+        Wash, rinse, repeat.
+      </li>
+    </ol>
+  </div>
+)
+
+export default HowTo
