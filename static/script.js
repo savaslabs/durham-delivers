9388e621b82b686a6157de93bf9311601ab21a94
C.C. Leonard <67921480+ccleonard@users.noreply.github.com>
2020-08-07 08:54:15 -0400

    Adjusted the script

--- static/script.js
+++ static/script.js
@@ -1,47 +1,44 @@
-window.onload = function() {
-  function checkAll1() {
+function checkAll1() {
 
-    const inputs = document.querySelectorAll('.checkbox');
-    for (let i = 0; i < inputs.length; i++) {
-      inputs[i].checked = true;
+  const inputs = document.querySelectorAll('.checkbox');
+  for (let i = 0; i < inputs.length; i++) {
+    inputs[i].checked = true;
+  }
+
+  this.onclick = uncheckAll1;
+}
+
+function uncheckAll1() {
+  const inputs = document.querySelectorAll('.checkbox');
+  for (let i = 0; i < inputs.length; i++) {
+    inputs[i].checked = false;
+  }
+
+  this.onclick = checkAll1;
+}
+
+const el = document.getElementById('menu');
+el.onclick = uncheckAll1;
+
+//use window.scrollY
+let scrollpos = window.scrollY;
+const header = document.getElementById('hamburger');
+
+function add_class_on_scroll() {
+    header.classList.add('scrolled');
+}
+
+function remove_class_on_scroll() {
+    header.classList.remove('scrolled');
+}
+
+window.addEventListener('scroll', function() {
+    scrollpos = window.scrollY;
+
+    if ( scrollpos > 10 ) {
+        add_class_on_scroll();
     }
-
-    this.onclick = uncheckAll1;
-  }
-
-  function uncheckAll1() {
-    const inputs = document.querySelectorAll('.checkbox');
-    for (let i = 0; i < inputs.length; i++) {
-      inputs[i].checked = false;
+    else {
+        remove_class_on_scroll();
     }
-
-    this.onclick = checkAll1;
-  }
-
-  const el = document.getElementById('menu');
-  el.onclick = checkAll1;
-
-  };
-
-  //use window.scrollY
-  let scrollpos = window.scrollY;
-  const header = document.getElementById('hamburger');
-
-  function add_class_on_scroll() {
-      header.classList.add('scrolled');
-  }
-
-  function remove_class_on_scroll() {
-      header.classList.remove('scrolled');
-  }
-
-  window.addEventListener('scroll', function() {
-      scrollpos = window.scrollY;
-
-      if ( scrollpos > 10 ) {
-          add_class_on_scroll();
-      }
-      else {
-          remove_class_on_scroll();
-      }
-  });
+});
