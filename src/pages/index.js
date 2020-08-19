217f8c779cae9bc52d27f00f07fcfb2b448df82f
Colleen Leonard <colleenleonard@colleens-mbp-2.lan>
2020-08-19 11:39:01 -0400

    Added 404 page and styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,24 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Hero from "../components/hero"
 import Warning from "../components/warning"
 import HowTo from "../components/how"
 import Locations from "../components/locations"
 import Restaurants from "../components/restaurants"
 import Accordion from "../components/accordion"
+import Footer from "../components/footer"
 
 const IndexPage = () => (
   <Layout>
       <SEO title="Home" />
+      <Hero />
       <Warning />
       <HowTo />
       <Locations />
       <Restaurants />
       <Accordion />
+      <Footer />
   </Layout>
 )
 
