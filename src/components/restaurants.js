380dc88cc15b56c317073864429830d6d5805a1c
maddybvh <maddybvh@gmail.com>
2020-09-03 19:29:28 -0400

    Change class to className

--- src/components/restaurants.js
+++ src/components/restaurants.js
@@ -1,25 +1,21 @@
-import React from "react"
-import DurhamImg from "../images/durham-img.jpg"
-import BlueCorn from "../images/blue-corn.jpg"
-import BullCity from "../images/bull-city-burger.png"
-import Copa from "../images/copa.png"
-import Dames from "../images/dames-chicken-and-waffles.png"
-import Goorsha from "../images/goorsha.jpg"
-import Parker from "../images/parker-and-otis.png"
-import Pompierei from "../images/pompierei.jpg"
-import Zweli from "../images/zweli.png"
-import Federal from "../images/federal.png"
-import Locopops from "../images/locopops.png"
+import React from "react";
+import DurhamImg from "../images/durham-img.jpg";
+import BlueCorn from "../images/blue-corn.jpg";
+import BullCity from "../images/bull-city-burger.png";
+import Copa from "../images/copa.png";
+import Dames from "../images/dames-chicken-and-waffles.png";
+import Goorsha from "../images/goorsha.jpg";
+import Parker from "../images/parker-and-otis.png";
+import Pompierei from "../images/pompierei.jpg";
+import Zweli from "../images/zweli.png";
+import Federal from "../images/federal.png";
+import Locopops from "../images/locopops.png";
 
 const Restaurants = () => (
   <div id="participating-restaurants">
     <div className="order__container">
-      <h2
-        data-sal="slide-up"
-        data-sal-delay="400"
-        data-sal-easing="ease-in"
-      >
-       Participating Restaurants
+      <h2 data-sal="slide-up" data-sal-delay="400" data-sal-easing="ease-in">
+        Participating Restaurants
       </h2>
       <div>
         <div className="container">
@@ -29,15 +25,19 @@ const Restaurants = () => (
             data-sal-easing="ease-in"
             className="location__item"
           >
-            <a href="https://bluecorncafedurham.com/" target="_blank" rel="noreferrer" className="restaurant__link">
-              <div className="restaurant__image"
+            <a
+              href="https://bluecorncafedurham.com/"
+              target="_blank"
+              rel="noreferrer"
+              className="restaurant__link"
+            >
+              <div
+                className="restaurant__image"
                 style={{
                   backgroundImage: `url(${BlueCorn})`,
                 }}
               >
-                <div class="restaurant__title">
-                  Blue Corn Café
-                </div>
+                <div className="restaurant__title">Blue Corn Café</div>
               </div>
             </a>
           </div>
@@ -47,13 +47,19 @@ const Restaurants = () => (
             data-sal-easing="ease-in"
             className="location__item"
           >
-            <a href="https://www.bullcityburgerandbrewery.com/" target="_blank" rel="noreferrer" className="restaurant__link">
-              <div className="restaurant__image"
+            <a
+              href="https://www.bullcityburgerandbrewery.com/"
+              target="_blank"
+              rel="noreferrer"
+              className="restaurant__link"
+            >
+              <div
+                className="restaurant__image"
                 style={{
                   backgroundImage: `url(${BullCity})`,
                 }}
               >
-                <div class="restaurant__title">
+                <div className="restaurant__title">
                   Bull City Burger & Brewery
                 </div>
               </div>
@@ -65,15 +71,19 @@ const Restaurants = () => (
             data-sal-easing="ease-in"
             className="location__item"
           >
-            <a href="https://copadurham.com/" target="_blank" rel="noreferrer" className="restaurant__link">
-              <div className="restaurant__image"
+            <a
+              href="https://copadurham.com/"
+              target="_blank"
+              rel="noreferrer"
+              className="restaurant__link"
+            >
+              <div
+                className="restaurant__image"
                 style={{
                   backgroundImage: `url(${Copa})`,
                 }}
               >
-                <div class="restaurant__title">
-                  Copa
-                </div>
+                <div className="restaurant__title">Copa</div>
               </div>
             </a>
           </div>
@@ -83,13 +93,19 @@ const Restaurants = () => (
             data-sal-easing="ease-in"
             className="location__item"
           >
-            <a href="https://dameschickenwaffles.com/" target="_blank" rel="noreferrer" className="restaurant__link">
-              <div className="restaurant__image"
+            <a
+              href="https://dameschickenwaffles.com/"
+              target="_blank"
+              rel="noreferrer"
+              className="restaurant__link"
+            >
+              <div
+                className="restaurant__image"
                 style={{
                   backgroundImage: `url(${Dames})`,
                 }}
               >
-                <div class="restaurant__title">
+                <div className="restaurant__title">
                   Dame's Chicken & Waffles
                 </div>
               </div>
@@ -101,15 +117,19 @@ const Restaurants = () => (
             data-sal-easing="ease-in"
             className="location__item"
           >
-            <a href="https://www.goorshadurham.com/" target="_blank" rel="noreferrer" className="restaurant__link">
-              <div className="restaurant__image"
+            <a
+              href="https://www.goorshadurham.com/"
+              target="_blank"
+              rel="noreferrer"
+              className="restaurant__link"
+            >
+              <div
+                className="restaurant__image"
                 style={{
                   backgroundImage: `url(${Goorsha})`,
                 }}
               >
-                <div class="restaurant__title">
-                  Goorsha
-                </div>
+                <div className="restaurant__title">Goorsha</div>
               </div>
             </a>
           </div>
@@ -119,15 +139,19 @@ const Restaurants = () => (
             data-sal-easing="ease-in"
             className="location__item"
           >
-            <a href="https://www.parkerandotis.com/" target="_blank" rel="noreferrer" className="restaurant__link">
-              <div className="restaurant__image"
+            <a
+              href="https://www.parkerandotis.com/"
+              target="_blank"
+              rel="noreferrer"
+              className="restaurant__link"
+            >
+              <div
+                className="restaurant__image"
                 style={{
                   backgroundImage: `url(${Parker})`,
                 }}
               >
-                <div class="restaurant__title">
-                  Parker & Otis
-                </div>
+                <div className="restaurant__title">Parker & Otis</div>
               </div>
             </a>
           </div>
@@ -137,15 +161,19 @@ const Restaurants = () => (
             data-sal-easing="ease-in"
             className="location__item"
           >
-            <a href="https://www.pompieripizza.com/" target="_blank" rel="noreferrer" className="restaurant__link">
-              <div className="restaurant__image"
+            <a
+              href="https://www.pompieripizza.com/"
+              target="_blank"
+              rel="noreferrer"
+              className="restaurant__link"
+            >
+              <div
+                className="restaurant__image"
                 style={{
                   backgroundImage: `url(${Pompierei})`,
                 }}
               >
-                <div class="restaurant__title">
-                  Pompieri Pizza
-                </div>
+                <div className="restaurant__title">Pompieri Pizza</div>
               </div>
             </a>
           </div>
@@ -155,15 +183,19 @@ const Restaurants = () => (
             data-sal-easing="ease-in"
             className="location__item"
           >
-            <a href="https://www.zwelis.com/" target="_blank" rel="noreferrer" className="restaurant__link">
-              <div className="restaurant__image"
+            <a
+              href="https://www.zwelis.com/"
+              target="_blank"
+              rel="noreferrer"
+              className="restaurant__link"
+            >
+              <div
+                className="restaurant__image"
                 style={{
                   backgroundImage: `url(${Zweli})`,
                 }}
               >
-                <div class="restaurant__title">
-                  Zweli's
-                </div>
+                <div className="restaurant__title">Zweli's</div>
               </div>
             </a>
           </div>
@@ -173,15 +205,19 @@ const Restaurants = () => (
             data-sal-easing="ease-in"
             className="location__item"
           >
-            <a href="https://ilovelocopops.com/" target="_blank" rel="noreferrer" className="restaurant__link">
-              <div className="restaurant__image"
+            <a
+              href="https://ilovelocopops.com/"
+              target="_blank"
+              rel="noreferrer"
+              className="restaurant__link"
+            >
+              <div
+                className="restaurant__image"
                 style={{
                   backgroundImage: `url(${Locopops})`,
                 }}
               >
-                <div class="restaurant__title">
-                  Locopops
-                </div>
+                <div className="restaurant__title">Locopops</div>
               </div>
             </a>
           </div>
@@ -191,27 +227,32 @@ const Restaurants = () => (
             data-sal-easing="ease-in"
             className="location__item"
           >
-            <a href="https://www.thefederal.net/" target="_blank" rel="noreferrer" className="restaurant__link">
-              <div className="restaurant__image"
+            <a
+              href="https://www.thefederal.net/"
+              target="_blank"
+              rel="noreferrer"
+              className="restaurant__link"
+            >
+              <div
+                className="restaurant__image"
                 style={{
                   backgroundImage: `url(${Federal})`,
                 }}
               >
-                <div class="restaurant__title">
-                  Federal
-                </div>
+                <div className="restaurant__title">Federal</div>
               </div>
             </a>
           </div>
         </div>
       </div>
     </div>
-    <div className="cover__image"
+    <div
+      className="cover__image"
       style={{
-        backgroundImage: `url(${DurhamImg})`
-      }}>
-    </div>
+        backgroundImage: `url(${DurhamImg})`,
+      }}
+    ></div>
   </div>
-)
+);
 
-export default Restaurants
+export default Restaurants;
