7f3320d6b90d12c97c0afd8bdc7ad3b1e312ae0d
maddybvh <maddybvh@gmail.com>
2020-09-05 08:03:26 -0400

    Issue #26928623: Content tweaks

--- src/components/accordion.js
+++ src/components/accordion.js
@@ -30,102 +30,6 @@ const Accordions = () => (
               className="accordion__component"
             >
               <div className="accordion__date">
-                Fri, Sept 4 <span style={{ margin: `0 15px` }}>|</span> 5:30 -
-                6:00 PM
-                <a
-                  href="https://calendar.google.com/event?action=TEMPLATE&tmeid=M3FhYzUyb21xOXR2cTRrc3MzZzNpbHZqOGUgY190MnVsYnQwNzdlOTIzZzZtM2QxdW5uaThtOEBn&tmsrc=c_t2ulbt077e923g6m3d1unni8m8%40group.calendar.google.com"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="calendar__btn"
-                  style={{
-                    marginLeft: `20px`,
-                  }}
-                >
-                  <Calendar />
-                </a>
-              </div>
-              <div className="accordion__order-time">
-                Order by Fri Sept. 4 @ 4 PM. Call in your order between 11:15
-                a.m.-4 p.m. on Friday, Sept. 4. Specify that it is for the
-                Apartments @ American Tobacco Durham Delivers order. Pick up in
-                lobby.
-              </div>
-              <div className="accordion__name">Old Bull at ATC</div>
-              <div className="accordion__address">
-                300 Blackwell St., Durham, NC 27701
-              </div>
-              <ul>
-                <li>
-                  <a
-                    href="https://www.bullcityburgerandbrewery.com/"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="accordion__link"
-                  >
-                    Bull City Burger & Brewery
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://www.pompieripizza.com/"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="accordion__link"
-                  >
-                    Pompieri Pizza
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div
-              data-sal="slide-up"
-              data-sal-easing="ease"
-              className="accordion__component"
-            >
-              <div className="accordion__date">
-                Fri, Sept 4 <span style={{ margin: `0 15px` }}>|</span> 5:30 -
-                6:00 PM
-                <a
-                  href="https://calendar.google.com/event?action=TEMPLATE&tmeid=MG05dTh0ZHE1bHZ0djRnNHYydmU3YmlzZW8gY190MnVsYnQwNzdlOTIzZzZtM2QxdW5uaThtOEBn&tmsrc=c_t2ulbt077e923g6m3d1unni8m8%40group.calendar.google.com"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="calendar__btn"
-                  style={{
-                    marginLeft: `20px`,
-                  }}
-                >
-                  <Calendar />
-                </a>
-              </div>
-              <div className="accordion__order-time">
-                Order by 10 PM on Thursday, Sept. 3. Order online for pickup,
-                schedule for 5:30 PM on 9/4. Include “Solis Brightleaf Durham
-                Delivers Order” as special instructions when you place your
-                order. Pick up food at lobby.
-              </div>
-              <div className="accordion__name">Solis Brightleaf Apartments</div>
-              <div className="accordion__address">
-                1005 W Main St. Durham, NC 27701
-              </div>
-              <ul>
-                <li>
-                  <a
-                    href="https://bluecorncafedurham.com/"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="accordion__link"
-                  >
-                    Blue Corn Café
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div
-              data-sal="slide-up"
-              data-sal-easing="ease"
-              className="accordion__component"
-            >
-              <div className="accordion__date">
                 Wednesday, Sept. 9 <span style={{ margin: `0 15px` }}>|</span>
                 12 - 12:30 PM
                 <a
@@ -141,13 +45,14 @@ const Accordions = () => (
                 </a>
               </div>
               <div className="accordion__order-time">
-                Order by: 10 p.m. on Tuesday, Sept. 8. Order for pickup, add
-                Durham Delivers menu option to cart, add location/day/time of
-                delivery to Special Instructions. Pick up location: American
-                Underground Lobby
+                Order by: 10 p.m. on Tuesday, Sept. 8. <br />
+                Order for pickup, add Durham Delivers menu option to cart, add
+                location/day/time of delivery to Special Instructions.
               </div>
               <div className="accordion__name">American Underground</div>
-              <div className="accordion__address">201 West Main St.</div>
+              <div className="accordion__address">
+                Lobby @ 201 West Main St.
+              </div>
               <ul>
                 <li>
                   <a
@@ -182,8 +87,16 @@ const Accordions = () => (
                 </a>
               </div>
               <div className="accordion__order-time">
-                Order by 10 p.m. Wednesday, Sept. 9. Orders MUST be placed via
-                the custom site linked below.
+                Order by 10 p.m. Wednesday, Sept. 9. <br />
+                Orders MUST be placed via this custom site:{" "}
+                <a
+                  href="http://ordering.app/federal"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="accordion__link"
+                >
+                  ordering.app/federal
+                </a>
               </div>
               <div className="accordion__name">Alden Place Apartments</div>
               <div className="accordion__address">
@@ -223,14 +136,17 @@ const Accordions = () => (
                 </a>
               </div>
               <div className="accordion__order-time">
+                Order by: 11 a.m. on Thursday, Sept. 10. <br />
                 Add “Group Order” item at the bottom of the menu to your cart.
-                Order by: 11 a.m. on Thursday, Sept. 10. Durham Delivers Apts @
-                ATC/Sept. 10/6 p.m.
+                For Special Instructions include “Durham Delivers Apts @ ATC /
+                Sept. 10 / 6 p.m.”
               </div>
               <div className="accordion__name">
                 Apartments @ American Tobacco
               </div>
-              <div className="accordion__address">300 Blackwell St.</div>
+              <div className="accordion__address">
+                Lobby @ 300 Blackwell St.
+              </div>
               <ul>
                 <li>
                   <a
@@ -266,14 +182,15 @@ const Accordions = () => (
                 </a>
               </div>
               <div className="accordion__order-time">
-                10 p.m. on Tuesday, Sept. 8. Complete the order form linked
-                below. As a Special Note at the bottom of the form, include
-                “Durham Delivers Audubon Park/Sept. 10/6:30 p.m.” Pick up at
-                Audubon Park Pool.
+                Order by: 10 p.m. on Tuesday, Sept. 8. <br />
+                Complete the order form linked below. As a Special Note at the
+                bottom of the form, include “Durham Delivers Audubon Park /
+                Sept. 10 / 6:30 p.m.”
               </div>
               <div className="accordion__name">Audubon Park</div>
               <div className="accordion__address">
-                Corner of Solitude Way & Whisperwind Dr., Durham, NC 27713
+                Audubon Park Pool @ Corner of Solitude Way & Whisperwind Dr.,
+                Durham, NC 27713
               </div>
               <ul>
                 <li>
