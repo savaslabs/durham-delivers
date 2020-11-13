c46f7dcb6f26d89c64f5537fcbb904956321dea2
Alex Manzo <manzoalex17@gmail.com>
2020-11-13 16:40:57 -0500

    Refactor menu open/close handling.

--- src/components/menuLink.js
+++ src/components/menuLink.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import ScrollIntoView from 'react-scroll-into-view'
+import ScrollIntoView from 'react-scroll-into-view';
 
-const MenuLink = ({ selector, text }) => (
-  <ScrollIntoView selector={selector}>
-    <a href={selector} onClick={(e) => e.preventDefault()}>
-      {text}
-    </a>
-  </ScrollIntoView>
-);
+const MenuLink = ({ selector, text }) => {
+  const handleMenuClick = e => {
+    e.preventDefault();
+    const input = document.querySelector('input.checkbox');
+    input.checked = false;
+    document.body.style.overflow = 'initial';
+  }
+
+  return (
+    <ScrollIntoView selector={selector}>
+      <a href={selector} onClick={(e) => handleMenuClick(e)}>
+        {text}
+      </a>
+    </ScrollIntoView>
+  );
+};
 
 export default MenuLink;
